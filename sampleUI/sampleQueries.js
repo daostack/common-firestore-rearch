65945bed1e75fb289c5fc723748f1db0e80ad469
Jason Berryman <jason@418.dev>
2021-12-08 19:45:04 +0000

    Screens 08.02

--- sampleUI/sampleQueries.js
+++ sampleUI/sampleQueries.js
@@ -1,15 +1,5 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs, doc, onSnapshot } from 'firebase/firestore/lite';
-// Follow this pattern to import other Firebase services
-// import { } from 'firebase/<service>';
-
-// TODO: Replace the following with your app's Firebase project configuration
-const firebaseConfig = {
-  //...
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
+const firebase = require('firebase');
+const db = firebase.firestore();
 
 // 2.5 Complete your account
   const userDoc = {
@@ -19,37 +9,77 @@ const db = getFirestore(app);
     intro: String
   };
   
-  const userRef = db.doc(`users/${user.id}`);
-  await userRef.set(userDoc);
+  await db.doc(`users/${user.id}`).set(userDoc);
 
 // 08.01 Account registered user empty states
-  const userRef = db.doc(`users/${user.id}`);
-  const commonsRef = db.collection('commons')
-    .where('activeMembers', 'array-conatins', user.id);
-  const proposalsRef = db.collectionGroup('proposals')
-    .where('proposer.id', '==', user.id);
-
+// 08.02.1 account registered user (my account view)
+// 08.02.2 account registered user scrolled proposals
+// 08.02.3 account registered user scroll
   // User document fetch with onSnapshot
-    let {
-    firstName = "", lastName = "", email = "", country = "", intro = "",
-    counts: {
-      commonsActive: commonsActiveCount = 0,
-      proposalsActive: proposalsActiveCount = 0
-    }
-  } = userDS.data();
+    const userRef = db.doc(`users/${user.id}`);
+
+    const unsubUser = userRef.onSnapshot((userDS) => {
+      let {
+        firstName = "", lastName = "", email = "", country = "", intro = "",
+        counts: {
+          commonsActive: commonsActiveCount = 0,
+          proposalsActive: proposalsActiveCount = 0
+        }
+      } = userDS.data();
+    });
 
   // Common document fetch with onSnapshot
-  let {
-    title = "", intro = "", totalRaised = 0, currency = "USD",
-    counts: {activeMembers: activeMembersCount = 0}
-  } = commonsQDS.data();
+  const commonsRef = db.collection('commons')
+    .where('activeMembers', 'array-conatins', user.id)
+    .limit(10);
+  const unsubCommons = commonsRef.onSnapshot((commonsQS => {
+    const commonsData = commonsQS.map((commonsQDS) => {
+      let {
+        title = "", intro = "", totalRaised = 0, currency = "USD",
+        counts: {activeMembers: activeMembersCount = 0}
+      } = commonsQDS.data();
+      
+      return {title, intro, totalRaised, currency, activeMembersCount};
+    })
+    console.log(commonsData);
+  }))
+
 
   // Proposal document fetch with onSnapshot
-  let {
-    title, proposer, totalAmount, currency, status,
-    counts: {
-      votesFor: votesForCount = 0,
-      votesAgainst: votesAgainstCount = 0,
-      discussions: discussionsCount = 0
-    } = proposalsQDS.data();
-  }
+  const proposalsRef = db.collectionGroup('proposals')
+    .where('proposer.id', '==', user.id)
+    .limit(10);
+  const unsubProposale = proposalsRef.onSnapshot((proposalsQS) => {
+    const proposalsData = proposalsQS.map((proposalsQDS) => {
+      let {
+        title = "",
+        proposer: {id = "", firstName = "", lastName = "", profilePic = ""},
+        totalAmount = 0, currency = "USD", status = "inactive",
+        counts: {
+          votesFor: votesForCount = 0,
+          votesAgainst: votesAgainstCount = 0,
+          discussions: discussionsCount = 0
+        } = proposalsQDS.data();
+      }
+      return {
+        title, proposer, totalAmount, currency, status,
+        votesForCount, votesAgainstCount, discussionsCount
+      };
+    })
+  })
+
+  // Membership request document fetch with onSnapshot
+  const commonsRef = db.collection('commons')
+    .where('pendingMembers', 'array-conatins', user.id)
+    .limit(10);
+  const unsubCommons = commonsRef.onSnapshot((commonsQS => {
+    const membershipRequestsData = commonsQS.map((commonsQDS) => {
+      let {
+        title = "", intro = "", totalRaised = 0, currency = "USD",
+        counts: {activeMembers: activeMembersCount = 0}
+      } = commonsQDS.data();
+      
+      return {title, intro, totalRaised, currency, activeMembersCount};
+    })
+    console.log(membershipRequestsData);
+  }))
