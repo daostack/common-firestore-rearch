2e8958ef61ef6ca6d4ff42fa40e014bd35ebf628
Amos Elmaliah <amosel@gmail.com>
2022-01-18 12:53:11 -0500

    update event types.

--- types/index.ts
+++ types/index.ts
@@ -14,9 +14,28 @@ export interface BaseRecord {
   updated: DocumentDateTime;
 }
 
-export interface History {
-  change: DocumentDateTime; // The time when the update was made
-  data: any; // An object containing the previous version of the document
+interface Change {
+  object: 'proposal', // expand this type
+  previous: any,
+  current: any
+}
+export interface Event {
+  utc: Timestamp; // The time that the update was made
+  eventType: 'create' | 'update' | 'delete' | 'orch'
+  // how to remove Create/Update/Delete events, and
+  // how to remove orchestrated events.
+  initiator: {
+    type: 'user',
+    uid: string
+  } | {
+    type: 'action',
+    requestedByUid: string
+  }
+  object: 'proposal' // expand this type
+  objectId: string,
+  type: 'accepted' | 'declined'
+  subtype: 'join' | 'funding'
+  previous: Record<string, Change>; // An object containing the previous version of the document
   _restore?: boolean; // Set this to 'TRUE' to restore this document version.
 }
 
